0753b0b81d7531cf865038a8c37b86885ac2c30f
Evgeny Mandrikov <mandrikov@gmail.com>
2012-06-02 11:07:06 +0000

    Add devkit

---
+++ flex-devkit/src/main/java/org/sonar/flex/devkit/FlexDevKit.java
@@ -0,0 +1,55 @@
+/*
+ * Sonar Flex Plugin
+ * Copyright (C) 2010 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.flex.devkit;
+
+import com.google.common.collect.ImmutableList;
+import com.sonar.sslr.devkit.SsdkGui;
+import com.sonar.sslr.impl.Parser;
+import org.sonar.colorizer.*;
+import org.sonar.flex.api.FlexGrammar;
+import org.sonar.flex.api.FlexKeyword;
+import org.sonar.flex.parser.FlexParser;
+
+import java.util.List;
+
+public final class FlexDevKit {
+
+  private FlexDevKit() {
+  }
+
+  public static void main(String[] args) {
+    System.setProperty("com.apple.mrj.application.apple.menu.about.name", "SSDK");
+    Parser<FlexGrammar> parser = FlexParser.create();
+    SsdkGui cppSsdkGui = new SsdkGui(parser, getTokenizers());
+    cppSsdkGui.setVisible(true);
+    cppSsdkGui.setSize(1000, 800);
+    cppSsdkGui.setTitle("Flex : Development Kit");
+  }
+
+  public static List<Tokenizer> getTokenizers() {
+    return ImmutableList.of(
+        new StringTokenizer("<span class=\"s\">", "</span>"),
+        new CDocTokenizer("<span class=\"cd\">", "</span>"),
+        new JavadocTokenizer("<span class=\"cppd\">", "</span>"),
+        new CppDocTokenizer("<span class=\"cppd\">", "</span>"),
+        new KeywordsTokenizer("<span class=\"k\">", "</span>", FlexKeyword.keywordValues()));
+  }
+
+}

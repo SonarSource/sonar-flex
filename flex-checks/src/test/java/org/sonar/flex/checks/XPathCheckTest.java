7bbf8320bc3fefe09c9d10bc1444ffde79b73bd8
Evgeny Mandrikov <mandrikov@gmail.com>
2012-06-05 12:18:27 +0600

    SONARPLUGINS-1905 Add generic Flex rule to define some homemade checks with an XPath expression

--- flex-checks/src/test/java/org/sonar/flex/checks/XPathCheckTest.java
+++ flex-checks/src/test/java/org/sonar/flex/checks/XPathCheckTest.java
@@ -19,26 +19,25 @@
  */
 package org.sonar.flex.checks;
 
-import com.google.common.collect.ImmutableList;
+import com.sonar.sslr.squid.checks.CheckMessagesVerifier;
+import org.junit.Test;
+import org.sonar.flex.FlexAstScanner;
+import org.sonar.squid.api.SourceFile;
 
-import java.util.List;
+import java.io.File;
 
-public final class CheckList {
+public class XPathCheckTest {
 
-  public static final String REPOSITORY_KEY = "flex";
+  @Test
+  public void check() {
+    XPathCheck check = new XPathCheck();
+    check.xpathQuery = "//IDENTIFIER[string-length(@tokenValue) >= 10]";
+    check.message = "Avoid identifiers which are too long!";
 
-  public static final String SONAR_WAY_PROFILE = "Sonar way";
-
-  private CheckList() {
-  }
-
-  public static List<Class> getChecks() {
-    return ImmutableList.<Class> of(
-        CommentRegularExpressionCheck.class,
-        LineLengthCheck.class,
-        NestedIfDepthCheck.class,
-        XPathCheck.class,
-        ParsingErrorCheck.class);
+    SourceFile file = FlexAstScanner.scanSingleFile(new File("src/test/resources/checks/Xpath.as"), check);
+    CheckMessagesVerifier.verify(file.getCheckMessages())
+        .next().atLine(2).withMessage("Avoid identifiers which are too long!")
+        .noMore();
   }
 
 }

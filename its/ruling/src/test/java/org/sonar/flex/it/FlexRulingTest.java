88df047e2aa18dde41ad2e6d9cc426a54704f7e4
Massimo Paladin <massimo.paladin@gmail.com>
2017-03-17 14:02:48 +0100

    Update lits to version 0.6

--- its/ruling/src/test/java/org/sonar/flex/it/FlexRulingTest.java
+++ its/ruling/src/test/java/org/sonar/flex/it/FlexRulingTest.java
@@ -37,7 +37,7 @@ public class FlexRulingTest {
   @ClassRule
   public static Orchestrator ORCHESTRATOR = Orchestrator.builderEnv()
     .addPlugin(FileLocation.byWildcardMavenFilename(new File("../../sonar-flex-plugin/target"), "sonar-flex-plugin-*.jar"))
-    .setOrchestratorProperty("litsVersion", "0.5")
+    .setOrchestratorProperty("litsVersion", "0.6")
     .addPlugin("lits")
     .restoreProfileAtStartup(FileLocation.of("src/test/resources/profile.xml"))
     .build();

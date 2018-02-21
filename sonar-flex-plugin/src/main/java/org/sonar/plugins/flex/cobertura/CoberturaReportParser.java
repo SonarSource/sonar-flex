6f936a0c28c6e4416550f1fe976fc504738eb719
Michael Gumowski <michael.gumowski@sonarsource.com>
2018-02-21 15:57:35 +0100

    SONARFLEX-95 Drop usage of deprecated StaxParser from SQ 5.6

--- sonar-flex-plugin/src/main/java/org/sonar/plugins/flex/cobertura/CoberturaReportParser.java
+++ sonar-flex-plugin/src/main/java/org/sonar/plugins/flex/cobertura/CoberturaReportParser.java
@@ -19,13 +19,20 @@
  */
 package org.sonar.plugins.flex.cobertura;
 
+import com.ctc.wstx.stax.WstxInputFactory;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import javax.annotation.CheckForNull;
+import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import org.apache.commons.lang.StringUtils;
+import org.codehaus.staxmate.SMInputFactory;
+import org.codehaus.staxmate.in.SMHierarchicCursor;
 import org.codehaus.staxmate.in.SMInputCursor;
 import org.sonar.api.batch.fs.FilePredicates;
 import org.sonar.api.batch.fs.FileSystem;
@@ -34,7 +41,6 @@ import org.sonar.api.batch.sensor.SensorContext;
 import org.sonar.api.batch.sensor.coverage.CoverageType;
 import org.sonar.api.batch.sensor.coverage.NewCoverage;
 import org.sonar.api.utils.ParsingUtils;
-import org.sonar.api.utils.StaxParser;
 import org.sonar.api.utils.log.Logger;
 import org.sonar.api.utils.log.Loggers;
 import org.sonar.plugins.flex.core.Flex;
@@ -50,11 +56,11 @@ public class CoberturaReportParser {
    * Parse a Cobertura xml report and create measures accordingly
    */
   public static void parseReport(File xmlFile, final SensorContext context) {
+    StaxParser parser = new StaxParser(rootCursor -> {
+      rootCursor.advance();
+      collectPackageMeasures(rootCursor.descendantElementCursor("package"), context);
+    });
     try {
-      StaxParser parser = new StaxParser(rootCursor -> {
-        rootCursor.advance();
-        collectPackageMeasures(rootCursor.descendantElementCursor("package"), context);
-      });
       parser.parse(xmlFile);
     } catch (XMLStreamException e) {
       throw new IllegalStateException(e);
@@ -79,11 +85,7 @@ public class CoberturaReportParser {
       if (inputFileByFilename.containsKey(fileName)) {
         inputFile = inputFileByFilename.get(fileName);
       } else {
-        String key = fileName.startsWith(File.separator) ? fileName : (File.separator + fileName);
-        inputFile = fileSystem.inputFile(predicates.and(
-          predicates.matchesPathPattern("file:**" + key.replace(File.separator, "/")),
-          predicates.hasType(InputFile.Type.MAIN),
-          predicates.hasLanguage(Flex.KEY)));
+        inputFile = findInputFile(fileSystem, predicates, fileName);
         inputFileByFilename.put(fileName, inputFile);
         if (inputFile == null && !fileName.endsWith(".mxml")) {
           LOG.warn("Cannot save coverage result for file: {}, because resource not found.", fileName);
@@ -105,6 +107,15 @@ public class CoberturaReportParser {
     }
   }
 
+  @CheckForNull
+  private static InputFile findInputFile(FileSystem fileSystem, FilePredicates predicates, String fileName) {
+    String key = fileName.startsWith(File.separator) ? fileName : (File.separator + fileName);
+    return fileSystem.inputFile(predicates.and(
+      predicates.matchesPathPattern("file:**" + key.replace(File.separator, "/")),
+      predicates.hasType(InputFile.Type.MAIN),
+      predicates.hasLanguage(Flex.KEY)));
+  }
+
   private static void collectFileData(SMInputCursor clazz, NewCoverage newCoverage) throws XMLStreamException {
     SMInputCursor line = clazz.childElementCursor("lines").advance().childElementCursor("line");
     while (line.getNext() != null) {
@@ -123,5 +134,50 @@ public class CoberturaReportParser {
       }
     }
     newCoverage.save();
+  }
+
+  /**
+   * StaxParser copied and simplified from deprecated StaxParser SQ API 5.6
+   */
+  private static class StaxParser {
+
+    private XmlStreamHandler streamHandler;
+    private SMInputFactory inf;
+
+    public StaxParser(XmlStreamHandler streamHandler) {
+      this.streamHandler = streamHandler;
+      XMLInputFactory xmlFactory = XMLInputFactory.newInstance();
+      if (xmlFactory instanceof WstxInputFactory) {
+        WstxInputFactory wstxInputfactory = (WstxInputFactory) xmlFactory;
+        wstxInputfactory.configureForLowMemUsage();
+        wstxInputfactory.getConfig().setUndeclaredEntityResolver((publicID, systemID, baseURI, namespace) -> namespace);
+      }
+      xmlFactory.setProperty(XMLInputFactory.IS_VALIDATING, false);
+      xmlFactory.setProperty(XMLInputFactory.SUPPORT_DTD, false);
+      xmlFactory.setProperty(XMLInputFactory.IS_NAMESPACE_AWARE, false);
+      inf = new SMInputFactory(xmlFactory);
+    }
+
+    public void parse(File xmlFile) throws XMLStreamException {
+      try (FileInputStream input = new FileInputStream(xmlFile)) {
+        parse(inf.rootElementCursor(input));
+      } catch (IOException e) {
+        throw new XMLStreamException(e);
+      }
+    }
+
+    private void parse(SMHierarchicCursor rootCursor) throws XMLStreamException {
+      try {
+        streamHandler.stream(rootCursor);
+      } finally {
+        rootCursor.getStreamReader().closeCompletely();
+      }
+    }
+
+  }
+
+  @FunctionalInterface
+  private interface XmlStreamHandler {
+    void stream(SMHierarchicCursor rootCursor) throws XMLStreamException;
   }
 }

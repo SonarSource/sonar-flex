a7088f2dee8cf65fa860c4404d57f87aa5fc7307
Massimo Paladin <massimo.paladin@gmail.com>
2019-06-13 16:31:56 +0200

    SONARFLEX-151 Rule S4524: "default" clauses should be first or last (#74)

---
+++ flex-checks/src/main/java/org/sonar/flex/checks/DefaultCasePositionCheck.java
@@ -0,0 +1,55 @@
+/*
+ * SonarQube Flex Plugin
+ * Copyright (C) 2010-2019 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.flex.checks;
+
+import com.sonar.sslr.api.AstNode;
+import com.sonar.sslr.api.AstNodeType;
+import java.util.Collections;
+import java.util.List;
+import org.sonar.check.Rule;
+import org.sonar.flex.FlexCheck;
+import org.sonar.flex.FlexGrammar;
+import org.sonar.flex.FlexKeyword;
+
+@Rule(key = "S4524")
+public class DefaultCasePositionCheck extends FlexCheck {
+
+  @Override
+  public List<AstNodeType> subscribedTo() {
+    return Collections.singletonList(FlexGrammar.SWITCH_STATEMENT);
+  }
+
+  @Override
+  public void visitNode(AstNode astNode) {
+    AstNode previous = null;
+    int nbCase = 0;
+    for (AstNode caseElement : astNode.getChildren(FlexGrammar.CASE_ELEMENT)) {
+      for (AstNode caseLabel : caseElement.getChildren(FlexGrammar.CASE_LABEL)) {
+        if (previous != null && nbCase > 1 && previous.getFirstChild().is(FlexKeyword.DEFAULT)) {
+          addIssue("Move this \"default\" clause to the beginning or end of this \"switch\" statement.", previous);
+          return;
+        }
+        previous = caseLabel;
+        ++nbCase;
+      }
+    }
+  }
+
+}

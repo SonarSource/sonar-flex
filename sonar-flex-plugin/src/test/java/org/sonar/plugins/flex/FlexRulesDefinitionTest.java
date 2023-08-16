6e8472f3d48d895c247b502f1314eb3d3f5404cf
Tibor Blenessy <saberduck@users.noreply.github.com>
2023-08-16 23:48:55 +0200

    Update sonar-plugin-api

--- sonar-flex-plugin/src/test/java/org/sonar/plugins/flex/FlexRulesDefinitionTest.java
+++ sonar-flex-plugin/src/test/java/org/sonar/plugins/flex/FlexRulesDefinitionTest.java
@@ -40,7 +40,7 @@ public class FlexRulesDefinitionTest {
     rulesDefinition.define(context);
     RulesDefinition.Repository repository = context.repository("flex");
 
-    assertThat(repository.name()).isEqualTo("SonarQube");
+    assertThat(repository.name()).isEqualTo("Sonar");
     assertThat(repository.language()).isEqualTo("flex");
     assertThat(repository.rules()).hasSize(CheckList.getChecks().size());
 
